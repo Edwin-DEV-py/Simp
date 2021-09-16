e3cd7a225eb26098b2da430fd55848248f0477c5
edwin-tarazona <nicolas.tarazona.morales@gmail.com>
2021-09-16 11:29:00 -0500

    vehiculosclases

---
+++ Clase_vehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsLibrary1
+{
+    public class Clase_vehiculo
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+
+        public static string Automovil = "AUTO";
+        public static string Bus = "BUS";
+        public static string Buseta = "BUSETA";
+        public static string Camiom = "CAMION";
+        public static string Camioneta = "CAMIONETA";
+        public static string Campero = "CAMPERO";
+        public static string Microbus = "MICROB";
+        public static string Tractocamion = "TRACTOC";
+        public static string Motocicleta = "MOTO";
+        public static string Motocarro = "MOTOCARRO";
+        public static string Mototriciclo = "MTRICICLO";
+        public static string Cuatrimoto = "CUATRIMOTO";
+        public static string Volqueta = "VOLQUETA";
+        public static string Otro = "OTRO";
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsLibrary1
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Placa { get; set;}
+        public string Marca { get; set; }
+        public string Linea { get; set; }
+        public string Combustible { get; set; }
+        public string Colores { get; set; }
+        public string Modelo { get; set; }
+        public string Cilindraje { get; set; }
+        public string Capacidad { get; set; }
+        public string Blindaje { get; set; }
+        public string Potencia { get; set; }
+        public Clase_vehiculo Tipo_vehiculo { get; set; }
+
+    }
+}
