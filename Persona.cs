6c68aff8df645dee38fab8bc99a1afb9fbdca074
edwin-tarazona <nicolas.tarazona.morales@gmail.com>
2021-09-16 11:04:56 -0500

    Primera clase

--- Persona.cs
+++ Persona.cs
@@ -4,6 +4,14 @@ namespace WinFormsLibrary1
 {
     public class Persona
     {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Primer_apellido { get; set; }
+        public string Segundo_apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Telefono { get; set; }
 
     }
 }
